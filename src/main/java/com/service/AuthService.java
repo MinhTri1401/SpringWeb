701f76a9fe263697cae474dcac1dd79c9c574c8c
MinhTri1401 <64025293+MinhTri1401@users.noreply.github.com>
2020-08-19 22:42:13 +0700

    Dich vu xac thuc tai khoan

--- src/main/java/com/service/AuthService.java
+++ src/main/java/com/service/AuthService.java
@@ -2,18 +2,47 @@ package com.service;
 
 import com.dto.RegisterRequest;
 import com.model.User;
+import com.model.VerificationToken;
+import com.repository.UserRepository;
+import com.repository.VerificationTokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 public class AuthService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private VerificationTokenRepository verificationTokenRepository;
+    @Transactional
     public void signup(RegisterRequest registerRequest){
         User user = new User();
         user.setUsername(registerRequest.getUsername());
         user.setEmail(registerRequest.getEmail());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setCreated(Instant.now());
         user.setEnabled(false);
+
+        userRepository.save(user);
+
+        generateVerificationtoToken(user);
+    }
+
+    private String generateVerificationtoToken(User user) {
+        //Tạo và lưu token gửi về mail khi tạo một tài khoản vào database
+        //Sau đó khi nhấn vào link gửi thì sẽ check user có tồn tại khôg
+        String token= UUID.randomUUID().toString();
+        VerificationToken verificationToken= new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 }
